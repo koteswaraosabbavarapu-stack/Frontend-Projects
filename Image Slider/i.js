f84ea75ae58d9582239eef26604c8cbbb5534af8
Kotesh <koteswaraosabbavarapu@gmail.com>
2025-12-29 18:56:18 +0530

    New pro

---
+++ Image Slider/i.js
@@ -0,0 +1,20 @@
+const slides=document.querySelector(".slides img");
+let slideIndex=0;
+let intervalId=null;
+
+
+function initializeSlider(){
+  slides[slideIndex].classList.add("displaySlide");
+
+}
+function showSlide(index){
+
+}
+function prevSlide(){
+
+}
+
+function nextSlide(){
+
+  
+}
